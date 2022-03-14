be8cb7d5bad50b437cb036bb4fe580e56bb00a09
brownef <frank.browne.87@gmail.com>
2022-03-14 17:28:52 +0100

    Plotting arrays now correctly zero-padded, and labelling automatic. Works to spec.

--- plot_sf.py
+++ plot_sf.py
@@ -6,21 +6,50 @@ import numpy as np
 import pandas as pd
 
 
-def zeropad(arr): # np[E,CS2], np[E,CS2]
-    # find longest length = max([(len(x),x) for x in ('a','b','aa')])
-    maxlen,maxele = max([(len(l),l) for l in arr])
-    print(maxlen,maxele)
-#
-#    arrS_zero = np.zeros(,dtype=float)
-#    iS = 0
-#    for iL in range(len(arrL)):
-#        if  iS<len(arrS):
-#            if arrL[iL][0] == arrS[iS][0]:
-#                arrS_zero[iL] = arrS[iS]
-#                iS+=1
-#
-#    return arrS_zero
-#
+def zeropad(arr):
+    # Get list of unique energies
+    e_list = []
+    for i in arr:
+        for j in i:
+            e_list.append(j[0])
+
+    e_list = sorted(set(e_list))
+    arr_zero = np.zeros([len(arr),len(e_list),2],dtype=float)
+
+    i_arr = 0
+    for i in arr_zero:
+        for e in range(len(e_list)):
+            i[e][0] = e_list[e]
+            tmparr = np.array(arr[i_arr])
+            tmp = np.where(tmparr[:,] == e_list[e])
+            tmplist = []
+            if tmp[1]!=1:
+                tmplist = list(tmparr[tmp[0]][tmp[1]])
+            if len(tmplist)>0:
+                i[e][1] = tmplist[0][1]
+        i_arr = i_arr + 1
+
+    return arr_zero
+
+def label(inlab): # 3digit string [nlj2]
+    # orbital angular momentum
+    l = 'X'
+    if inlab[1] == '0':
+        l = 's'
+    elif inlab[1] == '1':
+        l = 'p'
+    elif inlab[1] == '2':
+        l = 'd'
+    elif inlab[1] == '3':
+        l = 'f'
+    elif inlab[1] == '4':
+        l = 'g'
+    elif inlab[1] == '5':
+        l = 'h'
+    elif inlab[1] == '6':
+        l = 'i'
+
+    return '$\\nu'+inlab[0]+l+'_{'+inlab[2]+'/2}$'
 
 plt.rcParams["font.size"] = 15
 width = 0.100
@@ -62,6 +91,8 @@ else:
 # convert En bra to gs
 indf['En bra'] = indf['En bra'].map(lambda x: x+enbraOff)
 indf['En ket'] = indf['En ket'].map(lambda x: x+enketOff)
+indf.loc[indf['En ket']<1e-5,'En ket'] = 0 # because of floating point shenanigans
+indf.loc[indf['En bra']<1e-5,'En bra'] = 0 # because of floating point shenanigans
 
 # combine n,l,j2 columns to be nlj2
 indf['nlj2'] = indf['n'].astype(str)+indf['l'].astype(str)+indf['j2'].astype(str)
@@ -69,38 +100,29 @@ Nnlj2 = indf['nlj2'].nunique()
 innp = []
 nlj2lab = []
 for i_nlj in indf['nlj2'].unique():
-    tmpdf = indf[['En bra','CS^2']].loc[(indf['nlj2']==i_nlj)&(indf['En bra']>-0.1)&(indf['En bra']<5)&(indf['En ket']<1e-5)]
+    tmpdf = indf[['En bra','CS^2']].loc[(indf['nlj2']==i_nlj)&(indf['En bra']>-0.1)&(indf['En bra']<6)&(indf['En ket']==0)]
     tmpnp = tmpdf.to_numpy()
     nlj2lab.append(i_nlj)
     innp.append(tmpnp)
 
-print(innp)
-zeropad(innp)
+arr_zero = zeropad(innp)
+fix, ax = plt.subplots()
+opacity = 1
 
-# En ket<1e-5 for zero because getting to zero gives floating point thing
-leq3 = indf.loc[(indf['j2']==3)&(indf['En bra']>-0.1)&(indf['En bra']<5)&(indf['En ket']<1e-5)]
-leq1 = indf.loc[(indf['j2']==1)&(indf['En bra']>-0.1)&(indf['En bra']<5)&(indf['En ket']<1e-5)]
-leq3np = leq3[['En bra','CS^2']].to_numpy()
-leq1np = leq1[['En bra','CS^2']].to_numpy()
+bottom_arr = np.zeros(arr_zero[0].shape,float)
+i_lab = 0
+for arr_plot in arr_zero:
+    ax.bar(arr_plot[:,0],arr_plot[:,1],width,bottom=bottom_arr[:,1],label=label(nlj2lab[i_lab]))
+    bottom_arr = bottom_arr+arr_plot
+    i_lab = i_lab + 1
 
-# for all, need n, l, j2
-# need to find min and max of column
-# initalise nparray to size of
-
-#leq1np = zeropad(leq3np,leq1np)
-#
-#fix, ax = plt.subplots()
-#opacity = 1
-#
-#ax.bar(leq3np[:,0],leq3np[:,1],width,label=r'$\nu1p_{3/2}$')
-#ax.bar(leq1np[:,0],leq1np[:,1],width,bottom=leq3np[:,1],label=r'$\nu1p_{1/2}$')
-#
-indf.pivot_table(index='En bra',columns='nlj2',values='CS^2').plot.bar(rot=0, stacked=True,color = ['blue', 'green', 'red'])
-
+label(nlj2lab[2])
 plt.title(title)
 plt.xlabel('Energy [MeV]')
-plt.xlim([-0.1,5])
+plt.xlim([-0.1,6])
 plt.ylabel('$C^2S$')
 
 plt.legend()
 plt.savefig(savename, bbox_inches="tight")
+
+print("Finished "+title)
